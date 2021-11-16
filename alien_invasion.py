e777612ffb715be108fea8c76b70c4aa48711fbc
egor-or-love <egor.mpa@gmail.com>
2021-11-16 16:42:05 +0500

    Assigning a backgroud color.

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,16 +8,24 @@ class AlienInvasion:
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы."""
         pygame.init()
+
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+
+        # Назначение цвета фона.
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
             # Отслеживание событий клавиатуры и мыши.
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        sys.exit()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # При каждом проходе цикла перерисовывается экран.
+            self.screen.fill(self.bg_color)
+
             # Отображение последнего прорисованного экрана.
             pygame.display.flip()
 
