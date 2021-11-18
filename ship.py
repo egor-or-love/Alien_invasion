ea84feb174280a101dc0b8bced2239dc947407d7
egor-or-love <egor.mpa@gmail.com>
2021-11-18 17:20:21 +0500

    Spaceship speed regulation

--- ship.py
+++ ship.py
@@ -5,6 +5,7 @@ class Ship():
     def __init__(self, ai_game):
         """Инициализирует корабль и задает его начальную позицию."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Загружает изображение корабля и получает прямоугольник.
@@ -14,16 +15,23 @@ class Ship():
         # Каждый новый корабль появляется у нижнего края экрана.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Сохранение вещественной координаты центра корабля.
+        self.x = float(self.rect.x)
+
         # Флаги перемещения
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Обновляет позицию корабля с учетом флагов."""
+        # Обновляется атрибут x, не rect.
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # Обновление атрибута rect на основании self.x.
+        self.rect.x = self.x
 
     def blitme(self):
         """Рисует корабль в текущей позиции."""
